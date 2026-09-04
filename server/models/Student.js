bff02479f02266a9d4a1b044f0b45830fb02a273
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-04 22:26:32 +0500

    feat: update student model

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
+const historySchema = new mongoose.Schema({
+  date: { type: String, required: true },
+  attendance: { type: Boolean, default: false },
+  taskStatus: {
+    type: String,
+    enum: ["bajarilgan", "toliq bajarilmagan", "bajarilmagan"],
+    default: "bajarilmagan",
+  },
+});
+
 const studentSchema = new mongoose.Schema(
   {
     fullName: { type: String, required: true },
     course: { type: Number, required: true },
-    attendance: { type: Boolean, default: false },
-    taskStatus: {
-      type: String,
-      enum: ["bajarilgan", "toliq bajarilmagan", "bajarilmagan"],
-      default: "bajarilmagan",
-    },
+    history: [historySchema],
   },
   { timestamps: true },
 );
